62a196be025e6ecb02d090d7ba12de8be535818e
isnw2020-byte <isnw2020@gmail.com>
2026-02-24 13:08:47 +0900

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,5 @@
 
+const SUPABASE_URL  = 'https://rhcscvcgbquvmfgwwqrg.supabase.co';
+const SUPABASE_ANON = 'sb_publishable_oKJEgBYZL0m8j5b5dx1dyw_Y64824_2';
+const APP_VERSION = 'v2.0.0';
+const APP_NAME = '성남여성인력개발센터 출결시스템';
